a41ea63c681adedce748be17901e98934e2c9bf4
Sergey Ilinykh <62497831+un4g1vable@users.noreply.github.com>
2022-10-13 00:32:10 +0500

    Add files via upload

---
+++ 13.10 1task.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    setlocale(0, "");
+
+    int a, x, y;
+    cout << "Введи сторону: ";
+    cin >> a;
+    cout << "Введи координату точки по оси X: ";
+    cin >> x;
+    cout << "Введи координату точки по оси Y: ";
+    cin >> y;
+    if ((abs(x) < (a/2)) and (abs(y) < (a/2)))
+        cout << "Точка внутри квадрата";
+    else if ((abs(x) == (a / 2)) and (abs(y) < (a / 2)) or (abs(x) < (a / 2)) and (abs(y) == (a / 2)) or (abs(x) == (a / 2)) and (abs(y) == (a / 2)))
+        cout << "Точка на границе квадрата";
+    else
+        cout << "Точка вне квадрата";
+}
+
